4af1cf30763ba42de0c231c1d45df252605c69fa
Ivan Rubio <smashedpurse@gmail.com>
2023-02-20 15:23:27 -0600

    Develope the structure of all the app

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,25 @@ const App = () => (
           Navbar
         </div>
       </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats
+          Busisness
+          Billing
+          CardDeal
+          Testimonials
+          Clients
+          CTA
+          Footer
+        </div>
+      </div>
     </div>
   )
   )
